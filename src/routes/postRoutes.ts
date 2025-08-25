e0fadd303f62f8f48a2d35652fd881c600825b08
Manas Baranwal <72220768+Manasbrnwl@users.noreply.github.com>
2025-08-26 01:49:48 +0530

    authorize fixed for posts

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -13,15 +13,13 @@ import { authenticate, authorize } from "../middleware/authMiddleware";
 const router = express.Router();
 
 router.use(authenticate);
-router.use(authorize(["admin", "recruiter"]));
 
-router.post("/", createPosts);
-router.put("/update/:id", updatePost);
-router.delete("/delete/:id", deletePost);
-router.get("/get-all", getAllPosts);
-router.get("/:id", getPostById);
-router.get("/applied/get-all", getAppliedPosts);
-router.post("/apply/:id", applyToPost);
-
+router.post("/", authorize(["admin", "recruiter"]), createPosts);
+router.put("/update/:id", authorize(["admin", "recruiter"]), updatePost);
+router.delete("/delete/:id", authorize(["admin", "recruiter"]), deletePost);
+router.get("/get-all", authorize(["user"]), getAllPosts);
+router.get("/:id", authorize(["user"]), getPostById);
+router.get("/applied/get-all", authorize(["user"]), getAppliedPosts);
+router.post("/apply/:id", authorize(["user"]), applyToPost);
 
 export default router;
