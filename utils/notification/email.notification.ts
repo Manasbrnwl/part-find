1293b28796cc709fde54028f043d66c50d76d0f0
Manas Baranwal <baranwalmanas2903@gmail.com>
2026-06-05 17:54:23 +0530

    tried fixing the email issue

--- utils/notification/email.notification.ts
+++ utils/notification/email.notification.ts
@@ -9,25 +9,25 @@ dotenv.config();
 const transporter = nodemailer.createTransport(
   process.env.EMAIL_HOST
     ? {
-        host: process.env.EMAIL_HOST,
-        port: parseInt(process.env.EMAIL_PORT || "587"),
-        secure: process.env.EMAIL_SECURE === "true",
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        },
-        tls: {
-          ciphers: "SSLv3",
-          rejectUnauthorized: false,
-        },
-      }
+      host: process.env.EMAIL_HOST,
+      port: parseInt(process.env.EMAIL_PORT || "587"),
+      secure: process.env.EMAIL_SECURE === "true",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+      // tls: {
+      //   ciphers: "SSLv3",
+      //   rejectUnauthorized: false,
+      // },
+    }
     : {
-        service: "gmail",
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        },
-      }
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    }
 );
 
 /**
